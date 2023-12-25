3f1e71e6b9f84c1bb0539cd76b369c03bb5f4b99
guziakylbekova <guzalakylbekova@icloud.com>
2023-12-26 03:05:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import SecondImg from "./Component/SecondImg";
+import Text from "./Component/Text"
+import Price from "./Component/Price";
+import Stars from "./Component/Stars";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'container'}>
+        <div className="row">
+          <div className="col-6">
+            <div className="box">
+              <SecondImg/>
+            </div>
+          </div>
+          <div className="col-6 innerbox">
+            <Text/>
+            <Price/>
+            <div className="stars-wrap">
+              <Stars/>
+              <Stars/>
+              <Stars/>
+              <Stars/>
+            </div>
+          </div>
+        </div>
+      </div>
   );
-}
-
+};
 export default App;
